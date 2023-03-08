14c68a751b82cfc775ad680ff31f2213987c400b
codewithme224 <codewithme224@gmail.com>
2023-03-08 10:57:42 -0800

    Reverse ASCII

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    print("{:c}".format(i - 32 if i % 2 == 0 else i), end="")
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{:c}".format(c - i), end="")
+    i = 32 if i == 0 else 0
