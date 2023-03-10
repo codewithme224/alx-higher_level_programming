3935538aab63f3a0cd0571eb4b8ed7f4e0d25117
codewithme224 <codewithme224@gmail.com>
2023-03-10 04:26:10 -0800

    Updated

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    return (98 + a ** b)
+    from magic_calculation_102 import add, sub
+    if a < b:
+        result = add(a, b)
+        for i in range(2, 4):
+            result = add(result, i)
+        return (result)
+    else:
+        return(sub(a, b))
