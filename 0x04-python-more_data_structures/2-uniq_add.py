22c7627acf4ef7f13b8cdbd4d2230252833ae2c4
codewithme224 <codewithme224@gmail.com>
2023-03-15 15:09:23 -0700

    Fixes an error

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     unique_integers = set()
-    for element in my_list:
-        if isinstance(element, int):
-            if element not in unique_integers:
-                unique_integers.add(element)
-    return sum(unique_integers)
-
+    number = 0
+    for i in unique_integers:
+        number += i
+        return (number)
