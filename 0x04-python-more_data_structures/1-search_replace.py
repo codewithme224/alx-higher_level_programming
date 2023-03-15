0cc89df7f62223e88940d515ec9dd04e2029a2d0
codewithme224 <codewithme224@gmail.com>
2023-03-15 14:57:24 -0700

    Updated

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-def search_replace(my_list, search, replace):
-    new_list = []
+def uniq_add(my_list=[]):
+    unique_integers = set()
     for element in my_list:
-        if element == search:
-            new_list.append(replace)
-        else:
-            new_list.append(element)
-    return new_list
+        if isinstance(element, int):
+            if element not in unique_integers:
+                unique_integers.add(element)
+    return sum(unique_integers)
 
