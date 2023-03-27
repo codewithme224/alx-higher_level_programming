f4e4f70dc336005a362caca7d16aae110f843f12
codewithme224 <codewithme224@gmail.com>
2023-03-27 13:02:19 -0700

    Task added

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        for i in range(x):
-            print("{}".format(my_list[i]), end="")
-        print()
-        return x
-    except:
-        print()
-        return i
+    for i in range(x):
+        try:
+            print(my_list[i], end='')
+        except IndexError:
+            print()
+            return i
+    print()
+    return x
