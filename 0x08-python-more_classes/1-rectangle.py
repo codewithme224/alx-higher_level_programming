1465522d14c2da004b615effad7e2098d7d34825
codewithme224 <codewithme224@gmail.com>
2023-04-03 12:26:06 -0700

    Task added

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -14,30 +14,32 @@ class Rectangle:
         self.width = width
         self.height = height
 
-        @property
-        def width(self):
-            """Retrieves the width"""
-            return self.__width
+    @property
+    def width(self):
+        """Retrieves the width"""
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            """Sets the width"""
-            if isinstance(value, int) is False:
-                raise TypeError("width must be an integer")
-            if value < 0:
-                raise ValueError("width must be >= 0")
+    @width.setter
+    def width(self, value):
+        """Sets the width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
             self.__width = value
 
-        @property
-        def height(self, value):
-            """Retrieves the height"""
-            return self.__height
+    @property
+    def height(self, value):
+        """Retrieves the height"""
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            """Sets the height"""
-            if isinstance(value, int) is False:
-                raise TypeError("height must be an integer")
-            if value < 0:
-                raise ValueError("height must be >= 0")
+    @height.setter
+    def height(self, value):
+        """Sets the height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
             self.__height = value
