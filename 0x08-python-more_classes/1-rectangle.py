f8c552f29f445c752d3ee4a8c30ceb991d1976e3
codewithme224 <codewithme224@gmail.com>
2023-04-03 00:23:22 -0700

    First commt

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Module for Rectangle class"""
+
+
+class Rectangle:
+    """Class that defines a rectangle"""
+    def __init__(self, width=0, height=0):
+        """Initializes the data"""
+        self.width = width
+        self.height = height
+
+        @property
+        def width(self):
+            """Retrieves the width"""
+            return self.__width
+
+        @width.setter
+        def width(self, value):
+            """Sets the width"""
+            if type(value) is not int:
+                raise TypeError("width must be an integer")
+            elif value < 0:
+                raise ValueError("width must be >= 0")
+            else:
+                self.__width = value
+
+        @property
+        def height(self, value):
+            """Retrieves the height"""
+            return self.__height
+
+        @height.setter
+        def height(self, value):
+            """Sets the height"""
+            if type(value) is not int:
+                raise TypeError("height must be an integer")
+            elif value < 0:
+                raise ValueError("height must be >= 0")
+            else:
+                self.__height = value
