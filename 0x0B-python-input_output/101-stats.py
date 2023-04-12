9ba1f15cede34fbf6dfb049f852677a36e6dfa73
codewithme224 <codewithme224@gmail.com>
2023-04-12 13:14:17 -0700

    script that reads stdin line by line

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -3,31 +3,35 @@
 import sys
 
 if __name__ == "__main__":
-    size = 0
-    status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
-                    "403": 0, "404": 0, "405": 0, "500": 0}
-    count = 0
+    size = [0]
+    codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+
+    def check_match(line):
+        '''Checks for regexp match in line.'''
+        try:
+            line = line[:-1]
+            words = line.split(" ")
+            size[0] += int(words[-1])
+            code = int(words[-2])
+            if code in codes:
+                codes[code] += 1
+        except ValueError:
+            pass
+
+    def print_stats():
+        '''Prints accumulated statistics.'''
+        print("File size: {}".format(size[0]))
+        for k in sorted(codes.keys()):
+            if codes[k]:
+                print("{}: {}".format(k, codes[k]))
+    i = 1
     try:
         for line in sys.stdin:
-            if count == 10:
-                print("File size: {}".format(size))
-                for k, v in sorted(status_codes.items()):
-                    if v != 0:
-                        print("{}: {}".format(k, v))
-                count = 0
-            count += 1
-            try:
-                size += int(line.split()[-1])
-                status_codes[line.split()[-2]] += 1
-            except ValueError:
-                pass
+            check_match(line)
+            if i % 10 == 0:
+                print_stats()
+            i += 1
     except KeyboardInterrupt:
-        print("File size: {}".format(size))
-        for k, v in sorted(status_codes.items()):
-            if v != 0:
-                print("{}: {}".format(k, v))
+        print_stats()
         raise
-    print("File size: {}".format(size))
-    for k, v in sorted(status_codes.items()):
-        if v != 0:
-            print("{}: {}".format(k, v))
+    print_stats()
