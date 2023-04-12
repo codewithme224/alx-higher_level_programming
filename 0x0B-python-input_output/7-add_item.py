3777d6c154d18dbd9251b105a7142a1de298113a
codewithme224 <codewithme224@gmail.com>
2023-04-12 10:46:27 -0700

    Error fixed

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,23 +2,25 @@
 """Module for add_item method"""
 
 
-import sys
 import json
+import sys
 from os import path
-from sys import argv
 
 
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-
-filename = "add_item.json"
 my_list = []
 
+# Check if the file exists, and load the existing list if it does
+filename = 'add_item.json'
 if path.exists(filename):
     my_list = load_from_json_file(filename)
 
-for i in argv[1:]:
-    my_list.append(i)
+# Add all arguments to the list
+for arg in sys.argv[1:]:
+    my_list.append(arg)
 
+# Save the updated list to the file
 save_to_json_file(my_list, filename)
