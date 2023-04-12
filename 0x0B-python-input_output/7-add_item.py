ef03e82aba3997686042e963259beccd169c8ded
codewithme224 <codewithme224@gmail.com>
2023-04-12 10:32:57 -0700

    Error fixed

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -12,12 +12,13 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = \
         __import__('6-load_from_json_file').load_from_json_file
 
-json_file = "add_item.json"
+filename = "add_item.json"
 my_list = []
-if os.path.exists(json_file):
-    my_list = load_from_json_file(json_file)
+
+if os.path.exists(filename):
+    my_list = load_from_json_file(filename)
 
 for i in argv[1:]:
     my_list.append(i)
 
-save_to_json_file(my_list, json_file)
+save_to_json_file(my_list, filename)
