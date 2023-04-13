134d85ce818317acfdaae0ddb30c6b7b9cbfca48
codewithme224 <codewithme224@gmail.com>
2023-04-12 19:55:22 -0700

    Error fixed

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -5,30 +5,40 @@ if __name__ == "__main__":
     import sys
 
     size = 0
-    status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
+    status_codes = {}
+    valid_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
                     "403": 0, "404": 0, "405": 0, "500": 0}
     count = 0
+
     try:
         for line in sys.stdin:
             if count == 10:
                 print("File size: {}".format(size))
-                for k, v in sorted(status_codes.items()):
-                    if v != 0:
-                        print("{}: {}".format(k, v))
-                count = 0
-            count += 1
+                for key in sorted(status_codes):
+                    print("{}: {}".format(key, status_codes[key]))
+                count = 1
+            else:
+                count += 1
+            line = line.split()
             try:
-                size += int(line.split()[-1])
-                status_codes[line.split()[-2]] += 1
+                size += int(line[-1])
             except (IndexError, ValueError):
                 pass
+            try:
+                if line[-2] in valid_codes:
+                    if status_codes.get(line[-2], -1) == -1:
+                        status_codes[line[-2]] = 1
+                    else:
+                        status_codes[line[-2]] += 1
+            except IndexError:
+                pass
+
+        print("File size: {}".format(size))
+        for key in sorted(status_codes):
+            print("{}: {}".format(key, status_codes[key]))
+
     except KeyboardInterrupt:
         print("File size: {}".format(size))
-        for k, v in sorted(status_codes.items()):
-            if v != 0:
-                print("{}: {}".format(k, v))
+        for key in sorted(status_codes):
+            print("{}: {}".format(key, status_codes[key]))
         raise
-    print("File size: {}".format(size))
-    for k, v in sorted(status_codes.items()):
-        if v != 0:
-            print("{}: {}".format(k, v))
