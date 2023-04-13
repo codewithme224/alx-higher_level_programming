c5d38ab8992f4b334e82c45ad297ef2c74703096
codewithme224 <codewithme224@gmail.com>
2023-04-12 19:37:11 -0700

    Error fixed

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Module for stdin metrics"""
-import sys
-
 
 if __name__ == "__main__":
+    import sys
+
     size = 0
     status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
                     "403": 0, "404": 0, "405": 0, "500": 0}
