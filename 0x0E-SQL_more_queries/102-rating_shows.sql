0cd7a0d95be8721a433744409f82584427149acf
codewithme224 <codewithme224@gmail.com>
2023-05-17 15:20:56 -0700

    List all show

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,7 +1,7 @@
 -- script that lists all shows from hbtn_0d_tvshows_rate by their rating.
 SELECT `title`, SUM(`rate`) AS `rating`
-  FROM `tv_shows` AS t
-       INNER JOIN `tv_show_ratings` AS r
-       ON t.`id` = r.`show_id`
+  FROM `tv_shows` AS tv
+       INNER JOIN `tv_show_ratings` AS rt
+       ON tv.`id` = rt.`show_id`
  GROUP BY `title`
  ORDER BY `rating` DESC;
