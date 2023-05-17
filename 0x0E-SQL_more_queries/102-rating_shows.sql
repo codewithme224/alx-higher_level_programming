ed8dc2b472f206c2e0f0d89a867dff8b94def25a
codewithme224 <codewithme224@gmail.com>
2023-05-17 15:14:13 -0700

    List all show

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,6 @@
 -- script that lists all shows from hbtn_0d_tvshows_rate by their rating.
-SELECT tv_shows.title, SUM(rating) AS rating_sum
-FROM tv_shows
-LEFT JOIN ratings ON tv_shows.id = ratings.show_id
-GROUP BY tv_shows.title
-ORDER BY rating_sum DESC;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
