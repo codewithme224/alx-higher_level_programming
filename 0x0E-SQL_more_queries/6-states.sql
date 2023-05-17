973fa2b1e34e4760a4085ac4e280dbbde095206a
codewithme224 <codewithme224@gmail.com>
2023-05-17 15:31:42 -0700

    List all show

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 --Script that creates the database hbtn_0d_usa and the table states
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
-    PRIMARY KEY(`id`),
-    `id` INT UNIQUE NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(256) NOT NULL
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+	id INT UNIQUE AUTO_INCREMENT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY (id)
 );
