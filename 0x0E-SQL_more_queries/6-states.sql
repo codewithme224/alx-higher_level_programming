4c6ce10d675d50e18102e14f407c2c13958488d1
codewithme224 <codewithme224@gmail.com>
2023-05-17 17:53:50 -0700

    Task added

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,7 @@
 --Script that creates the database hbtn_0d_usa and the table states
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
--- use a database
-USE hbtn_0d_usa;
--- creates a table
-CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
+    PRIMARY KEY(`id`),
+    `id`   INT          NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(256) NOT NULL
+);
