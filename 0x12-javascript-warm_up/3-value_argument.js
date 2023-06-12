d568a66fbdb73f4c73dc5e9c10436105c966db3c
codewithme224 <codewithme224@gmail.com>
2023-06-12 15:49:52 -0700

    Task added

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,4 +1,2 @@
 #!/usr/bin/node
-const argument = process.argv[2];
-const result = typeof argument === 'undefined' ? 'No argument' : argument;
-console.log(result);
+console.log(process.argv[2] === undefined ? 'No argument' : process.argv[2]);
