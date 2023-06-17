aa3ecbc742a5460df86546133002189b92e75218
codewithme224 <codewithme224@gmail.com>
2023-06-17 15:04:40 -0700

    first commit

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(
             user=sys.argv[1],
             password=sys.argv[2],
-            database=sys.argv[3])
+            db=sys.argv[3])
 
     cur = db.cursor()
     cur.execute("SELECT * FROM `states`")
