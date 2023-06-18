c11ed8f462d1a5342379020ad5f0d61f2270924f
codewithme224 <codewithme224@gmail.com>
2023-06-18 07:14:21 -0700

    Task added

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module to list all states from database hbtn_0e_0_usa"""
+
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base
+
+
+class City(Base):
+    """Class representing the cities table"""
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, autoincrement=True,
+                nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
