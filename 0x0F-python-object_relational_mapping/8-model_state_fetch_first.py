8a6b464f44be34687f63e446e24c774d912f5fba
codewithme224 <codewithme224@gmail.com>
2023-06-18 06:05:50 -0700

    Task added

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -10,10 +10,11 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     """Connect to MySQL database"""
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]))
 
     Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
     session = Session()
 
     states = session.query(State).order_by(State.id).first()
