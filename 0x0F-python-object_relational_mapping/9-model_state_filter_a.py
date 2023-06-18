477b0fb4933590bee8c83b2fea8bf6cfc46fade4
codewithme224 <codewithme224@gmail.com>
2023-06-18 06:17:05 -0700

    Task added

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module to list all states from database hbtn_0e_0_usa"""
+
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    """Connect to MySQL database"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state in session.query(State).filter(State.name.like('%a%')).order_by(
+            State.id).all():
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()
