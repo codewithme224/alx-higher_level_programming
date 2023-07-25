87809579682be1eccafdcfd875874dd2c92d1344
codewithme224 <codewithme224@gmail.com>
2023-07-25 22:40:10 +0000

    script that reads and prints the content of a file.

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,17 +1,11 @@
 #!/usr/bin/node
 
-const request = require('request');
 const fs = require('fs');
-const url = process.argv[2];
-request(url, function (err, response, body) {
+
+fs.readFile(process.argv[2], 'utf-8', function (err, result) {
   if (err) {
     console.log(err);
   } else {
-    fs.writeFile(process.argv[3], body, 'utf-8', function (err) {
-      if (err) {
-        console.log(err);
-      }
-    });
+    console.log(result);
   }
-}
-);
+});
