1d22e912b277773cff5067e80bcd3a84d6161edf
codewithme224 <codewithme224@gmail.com>
2023-07-25 23:19:10 +0000

    script that computes the number of tasks completed by user id.

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,26 +1,21 @@
 #!/usr/bin/node
 
 const request = require('request');
-if (!process.argv || process.argv.length < 3) {
-  console.log('Missing arguments');
-  process.exit(1);
-}
-request(process.argv[2], function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    const results = JSON.parse(body);
-    const completed = {};
-    for (let i = 0; i < results.length; i++) {
-      if (results[i].completed) {
-        if (completed[results[i].userId]) {
-          completed[results[i].userId]++;
-        } else {
-          completed[results[i].userId] = 1;
-        }
+request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  const dict = JSON.parse(body).reduce((acc, elem) => {
+    if (!acc[elem.userId]) {
+      if (elem.completed) {
+        acc[elem.userId] = 1;
+      }
+    } else {
+      if (elem.completed) {
+        acc[elem.userId] += 1;
       }
     }
-    console.log(completed);
-  }
-}
-);
+    return acc;
+  }, {});
+  console.log(dict);
+});
