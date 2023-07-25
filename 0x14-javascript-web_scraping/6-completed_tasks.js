f253709bf32167c5db4f6b7d551899a5a0e2acdc
codewithme224 <codewithme224@gmail.com>
2023-07-25 23:28:34 +0000

    script that computes the number of tasks completed by user id.

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,34 +1,24 @@
 #!/usr/bin/node
 
 const request = require('request');
+const url = process.argv[2];
+const myDict = {};
 
-const apiUrl = process.argv[2];
-
-request(apiUrl, (err, response, body) => {
+request(url, function (err, data, body) {
   if (err) {
     console.log(err);
-    return;
-  }
+  } else {
+    const response = JSON.parse(body);
 
-  const todos = JSON.parse(body);
-
-  const completedTasks = todos.filter((todo) => todo.completed);
-
-  const users = new Map();
-
-  completedTasks.forEach((todo) => {
-    const userId = todo.userId;
-
-    if (!users.has(userId)) {
-      users.set(userId, 0);
-    }
-
-    users.set(userId, users.get(userId) + 1);
-  });
-
-  for (const [userId, numCompletedTasks] of users.entries()) {
-    if (numCompletedTasks > 0) {
-      console.log(`User ID: ${userId}, Tasks Completed: ${numCompletedTasks}`);
+    for (let i = 0; i < response.length; i++) {
+      if (response[i].completed === true) {
+        if (myDict[response[i].userId] === undefined) {
+          myDict[response[i].userId] = 1;
+        } else {
+          myDict[response[i].userId] += 1;
+        }
+      }
     }
   }
+  console.log(myDict);
 });
