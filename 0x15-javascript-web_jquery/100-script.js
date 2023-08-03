cc8500cac16e53202d89ace9ab562bee6f154b5b
codewithme224 <codewithme224@gmail.com>
2023-08-02 17:18:28 -0700

    Task added

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,7 @@
+document.activeElement('DOMContentLoaded', function () {
+  const headerElement = document.querySelector('header');
+
+  if (headerElement) {
+    headerElement.style.color = '#FF0000';
+  }
+});
